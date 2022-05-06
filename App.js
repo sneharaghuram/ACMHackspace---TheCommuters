bef3ace5315e9ea7caf9b4ff5c04d2c08328066b
Sneha Raghuram <sneha06@g.ucla.edu>
2022-05-05 20:07:44 -0700

    Made the Add Trip Screen

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// import HomeScreen from './components/HomeScreen';
+import ProfileScreen from './components/ProfileScreen';
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+     
+      <ProfileScreen />
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     width: '100%',
+//     height: "100%",
+//     backgroundColor: '#CAFFF2',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
---
+++ components/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+import {SafeAreaView} from 'react-native';
+
+function Homescreen() {
+    return (
+      <SafeAreaView>
+        <Text>'Welcome to AppName'</Text>;
+      </SafeAreaView>
+    );
+  } 
+export default HomeScreen;
